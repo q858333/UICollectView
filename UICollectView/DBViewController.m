dd25c4ee2124114a973951afc31b8b99994b9048
q858333 <284825870@qq.com>
2014-09-04 17:57:03 +0800

    test

--- UICollectView/DBViewController.m
+++ UICollectView/DBViewController.m
@@ -17,13 +17,89 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
+    
+    
+    UICollectionViewFlowLayout *flowLayout=[[UICollectionViewFlowLayout alloc] init];
+    [flowLayout setScrollDirection:UICollectionViewScrollDirectionVertical];
+    UICollectionView *collectionView=[[UICollectionView alloc]initWithFrame:CGRectMake(0, 0, 320, 480)collectionViewLayout:flowLayout];
+    collectionView.delegate=self;
+    [collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"CollectionCell"];
+    collectionView.dataSource=self;
+    
+    [self.view addSubview:collectionView];
+   // char a[10]=
+    NSString *str=@"sadasd123";
 
+    char *b=[str cString];
+    char a[str.length];
+    strcpy(a, b);
+    for(int i=0;i<strlen(a);i++)
+    {
+        
+        printf("%c",a[i]);
+    }
+     KL(a);
+    printf("%s",a);
+    NSString *s=[[NSString alloc] initWithCString:a encoding:NSUTF8StringEncoding];
+    NSLog(@"%@",s);
+    for(int i=0;i<strlen(a);i++)
+    {
+        
+    }
+}
+void KL (char *inStr)
+{
+    for(int i=0;i<strlen(inStr);i++)
+    {
+        inStr[i]=inStr[i]^'t';
+        
+        printf("%c",inStr[i]);
+    }
+}
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    
+    return 10;
+}
+-(NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
+{
+    return 1;
+}
+// The cell that is returned must be retrieved from a call to -dequeueReusableCellWithReuseIdentifier:forIndexPath:
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString * CellIdentifier = @"CollectionCell";
+    UICollectionViewCell * cell = [collectionView dequeueReusableCellWithReuseIdentifier:CellIdentifier forIndexPath:indexPath];
+    UILabel *lab=[[UILabel alloc]initWithFrame:CGRectMake(0, 0, 80, 50)];
+    lab.text=@"1111";
+    [cell addSubview:lab];
+    cell.backgroundColor = [UIColor colorWithRed:((10 * indexPath.row) / 255.0) green:((20 * indexPath.row)/255.0) blue:((30 * indexPath.row)/255.0) alpha:1.0f];
+    
+    return cell;
+}
+#pragma mark --UICollectionViewDelegateFlowLayout
+//定义每个UICollectionView 的大小
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    return CGSizeMake(96, 100);
+}
+//设置顶部的大小
+//-(CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout referenceSizeForHeaderInSection:(NSInteger)section{
+//    CGSize size={50,50};
+//    return size;
+//}
+//定义每个UICollectionView 的 margin
+-(UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout insetForSectionAtIndex:(NSInteger)section
+{
+    return UIEdgeInsetsMake(5, 5, 5, 5);
+}
 
+-(void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath{
+    NSLog(@"%@",indexPath);
+   }
 @end
